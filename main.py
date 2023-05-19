b015f5920f8abd2ad04909210fe466fcbfee981e
adamheidrick <adamheidrick@gmail.com>
2023-05-19 14:59:00 +0200

    Added content, style, and navigation.

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+import pyfiglet
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder="templateFiles", static_folder="staticFiles")
+title = pyfiglet.figlet_format("TEXT BASED ADVENTURE")
 
 
-@app.route("/")
-def hello_world():
-    return render_template('index.html')
+@app.route("/", methods=['Get', 'POST'])
+def home():
+    if request.method == 'GET':
+        return render_template('index.html', title=title)
 
 
 if __name__ == '__main__':
